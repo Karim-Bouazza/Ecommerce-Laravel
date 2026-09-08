76fe2a32e00430d58c32c48229035c7161c68932
BOUAZZA karim <163033054+Karim-Bouazza@users.noreply.github.com>
2026-09-08 17:51:44 +0100

    fix: fix delete portfeuil

--- backend/app/Services/Wallets/DeleteWalletService.php
+++ backend/app/Services/Wallets/DeleteWalletService.php
@@ -9,14 +9,6 @@ class DeleteWalletService
 {
     public function execute(Wallet $wallet): void
     {
-        if ($wallet->balance !== 0) {
-            throw new RuntimeException('Impossible de supprimer un portefeuille dont le solde n\'est pas nul.');
-        }
-
-        if ($wallet->transactions()->exists()) {
-            throw new RuntimeException('Impossible de supprimer un portefeuille ayant des transactions.');
-        }
-
         $wallet->delete();
     }
 }
